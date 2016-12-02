bad2b84d9054727b394a87badb62cff8a28c5d2b
Nirupama Prasad <nirupamaprasad@Nirupamas-MacBook-Air.local>
2016-12-02 07:03:17 -0800

    loginxml and signup xml

---
+++ MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.test.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        final TextView textView = (TextView) findViewById(R.id.link_signup);
+
+        textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+        };
+}
